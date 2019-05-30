6e7869f488eaf37eea308e9be9099982e95e1a95
cesar <omar18pe@gmail.com>
2019-05-30 12:41:04 -0500

    configurando express,creando el enlace al puerto

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-console.log('Works')
-hols
-sfsdfsdfsdf
+import express, { application, Application } from "express";
+
+class Server {
+
+    public app: Application;
+    constructor(){
+        this.app = express();
+    }
+
+    config(): void {
+        this.app.set('port', process.env.PORT || 3000)
+    } 
+    routes(): void {
+    
+    }
+    start() {
+        this.app.listen(this.app.get('port'), () => {
+            console.log('server on port', this.app.get('port'));
+        })
+    }
+}
+new Server();
